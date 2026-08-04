44bb33547b1e360b1d256bc9319343f681c776bc
SHAHAN.A <alangadanshahan@ug.cusat.ac.in>
2026-08-04 18:03:33 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=val)
+            {
+            
+            nums[k]=nums[i];
+            k++;  
+            }
+            else
+            {
+            continue;
+            }
+        }
+        return k;
+    }
+}
