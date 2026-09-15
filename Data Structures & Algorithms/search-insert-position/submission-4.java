226339b1ad6971e85d282faa72d3fe8fe4dd0d5e
SHAHAN.A <alangadanshahan@ug.cusat.ac.in>
2026-09-15 20:04:04 +0530

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        int mid=0;
+        while(left<=right)
+        {
+            mid=(left+right)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                left=mid+1;
+
+            }
+            else if (target<nums[mid])
+            {
+                right=mid-1;
+            }
+           
+        }
+     return left;
+    }
+}
+
+
+    
